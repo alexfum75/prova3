87f955103216a544c3aa463762a4a4198e325db6
Alessandro <quarto-github-actions-publish@example.com>
2026-09-11 16:22:07 +0200

    book

--- python/replace_strings.py
+++ python/replace_strings.py
@@ -1,18 +1,43 @@
 from pathlib import Path
 
-OLD_STRING = "Perche'"
-NEW_STRING = "Perché"
+SOSTITUZIONI = {
+    "Perche'": "Perché",
+    "perche'": "perché",
+    "E'": "È",
+    "caffe'": "caffè",
+    "universita'": "università",
+    "a'": "à",
+    "e'": "è",
+    "ne'": "né",
+    "se'": "sé"
+}
 
 print("Working on qmd files")
 for path in Path(".").rglob("*.qmd"):
     content = path.read_text(encoding="utf-8")
-    if OLD_STRING in content:
-        path.write_text(content.replace(OLD_STRING, NEW_STRING), encoding="utf-8")
-        print(f"Replaced {OLD_STRING} in: {path}")
+    modificato = False
+    
+    # Cicla sulla mappa ed esegue le sostituzioni
+    for old_string, new_string in SOSTITUZIONI.items():
+        if old_string in content:
+            content = content.replace(old_string, new_string)
+            print(f"Replaced '{old_string}' with '{new_string}' in: {path}")
+            modificato = True
+            
+    if modificato:
+        path.write_text(content, encoding="utf-8")
 
-print("Working on yml files")
+print("\nWorking on yml files")
 for path in Path(".").rglob("_quarto.yml"):
     content = path.read_text(encoding="utf-8")
-    if OLD_STRING in content:
-        path.write_text(content.replace(OLD_STRING, NEW_STRING), encoding="utf-8")
-        print(f"Replaced {OLD_STRING} in: {path}")
+    modificato = False
+    
+    # Cicla sulla mappa ed esegue le sostituzioni
+    for old_string, new_string in SOSTITUZIONI.items():
+        if old_string in content:
+            content = content.replace(old_string, new_string)
+            print(f"Replaced '{old_string}' with '{new_string}' in: {path}")
+            modificato = True
+            
+    if modificato:
+        path.write_text(content, encoding="utf-8")
